b8760537b7ce73ab56c3d01d503843eef89b9b3b
Mike Redrobe <mike@redrobe.net>
2020-02-09 11:42:56 +0000

    Update for API changes

--- starfield.cpp
+++ starfield.cpp
@@ -5,53 +5,53 @@
 using namespace blit;
 
 typedef struct star {
-    vec2 velocity;
-    vec2 pos;
+    Vec2 velocity;
+    Vec2 pos;
 } star;
 
 std::vector<star> stars;
 
 typedef struct shape {
-    std::vector<vec2> points;
-    vec2 pos;
-    vec2 dir;
-    vec3 color;
+    std::vector<Vec2> points;
+    Vec2 pos;
+    Vec2 dir;
+    Vec3 color;
     float size;
 } shape;
 
 shape alien1;
 
 void center_star( star &s) {
-    vec2 center = vec2 (fb.bounds.w / 2, fb.bounds.h / 2);
+    Vec2 center = Vec2 (screen.bounds.w / 2, screen.bounds.h / 2);
     s.pos = center;
-    s.velocity = vec2 (rand() % 100 - 50, rand() % 100 - 50) ;
+    s.velocity = Vec2 (rand() % 100 - 50, rand() % 100 - 50) ;
 }
 
-void draw_cross(vec2 pos) {
-    fb.pen(rgba(255,255,255));
-    fb.line(pos + vec2 (-10, -10), pos + vec2 (10, 10) );
-    fb.line(pos + vec2 (10, -10), pos + vec2 (-10, 10) );
+void draw_cross(Vec2 pos) {
+    screen.pen(RGBA(255,255,255));
+    screen.line(pos + Vec2 (-10, -10), pos + Vec2 (10, 10) );
+    screen.line(pos + Vec2 (10, -10), pos + Vec2 (-10, 10) );
 }
 
-void draw_laser(vec2 pos) {
-    fb.pen(rgba(rand() % 255,rand() % 255,rand() % 255));
-    fb.line(vec2 (0, fb.bounds.h), pos);
-    fb.line(vec2 (fb.bounds.w, fb.bounds.h), pos);
+void draw_laser(Vec2 pos) {
+    screen.pen(RGBA(rand() % 255,rand() % 255,rand() % 255));
+    screen.line(Vec2 (0, screen.bounds.h), pos);
+    screen.line(Vec2 (screen.bounds.w, screen.bounds.h), pos);
 }
-void draw_shape(shape shape,vec2 pos,float size){
-    fb.pen(rgba((int)shape.color.x,(int)shape.color.y,(int)shape.color.z));
-    vec2 lastpos = shape.points[0];
+void draw_shape(shape shape,Vec2 pos,float size){
+    screen.pen(RGBA((int)shape.color.x,(int)shape.color.y,(int)shape.color.z));
+    Vec2 lastpos = shape.points[0];
     for (auto &p: shape.points) {
-        vec2 point = p * size;
-        fb.line(lastpos + pos , point + pos);
+        Vec2 point = p * size;
+        screen.line(lastpos + pos , point + pos);
         lastpos = point;
     }
 }
 void new_alien () {
-    alien1.color = vec3 (rand() % 255,rand() % 255,rand() % 255);
-    vec2 center = vec2(fb.bounds.w / 2, fb.bounds.h / 2);
+    alien1.color = Vec3 (rand() % 255,rand() % 255,rand() % 255);
+    Vec2 center = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
     alien1.pos = center;
-    alien1.dir = vec2(rand() % 6 - 3 ,rand() % 6 - 3);
+    alien1.dir = Vec2(rand() % 6 - 3 ,rand() % 6 - 3);
 
     alien1.size = 0.1;
 }
@@ -59,19 +59,20 @@ void move_alien() {
     alien1.pos = alien1.pos + alien1.dir;
     alien1.size = alien1.size + 0.02;
     if ( alien1.size > 2) new_alien();
-    if ((alien1.pos.x < 0)  || (alien1.pos.x > fb.bounds.w) || (alien1.pos.y < 0 ) || (alien1.pos.y > fb.bounds.h))
+    if ((alien1.pos.x < 0)  || (alien1.pos.x > screen.bounds.w) || (alien1.pos.y < 0 ) || (alien1.pos.y > screen.bounds.h))
         new_alien();
 }
 int low_res, multicolor,hyperspace,fire,speed;
 
 void init() {
-    set_screen_mode(screen_mode::hires);
+    set_screen_mode(ScreenMode::hires);
 
-    alien1.points.push_back(vec2(0,0));
-    alien1.points.push_back(vec2(10,10));
-    alien1.points.push_back(vec2(20,0));
-    alien1.points.push_back(vec2(0,0));
+    alien1.points.push_back(Vec2(0,0));
+    alien1.points.push_back(Vec2(10,10));
+    alien1.points.push_back(Vec2(20,0));
+    alien1.points.push_back(Vec2(0,0));
     new_alien();
+
     for (int i=0; i < 200; i++ ) {
         star s;
         center_star(s);
@@ -80,16 +81,15 @@ void init() {
     low_res = hyperspace = fire = 0;
     speed = 50;
 }
-
 void render(uint32_t time) {
-vec2 center,move,joypos;
+Vec2 center,move,joypos;
 
     if (!hyperspace) {
-        fb.pen(rgba(0, 0, 0));
-        fb.clear();
+        screen.pen(RGBA(0, 0, 0));
+        screen.clear();
         }
 
-    fb.pen(rgba(255,255,255));
+    screen.pen(RGBA(255,255,255));
 
     for (auto &s : stars) {
 
@@ -99,15 +99,15 @@ vec2 center,move,joypos;
         s.velocity.x *= 1.1f;
         s.velocity.y *= 1.1f;
 
-        if ((s.pos.x < 0)  || (s.pos.y < 0) || (s.pos.x > fb.bounds.w)  ||  (s.pos.y > fb.bounds.h)) { center_star(s); }
+        if ((s.pos.x < 0)  || (s.pos.y < 0) || (s.pos.x > screen.bounds.w)  ||  (s.pos.y > screen.bounds.h)) { center_star(s); }
 
-        fb.pixel(s.pos);
+        screen.pixel(s.pos);
         }
 
     move_alien();
     if (!hyperspace ) draw_shape (alien1, alien1.pos,alien1.size);
 
-    center = vec2(fb.bounds.w / 2, fb.bounds.h / 2);
+    center = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
     move = blit::joystick;
 
     joypos.x =  move.x * center.x ;
@@ -115,7 +115,7 @@ vec2 center,move,joypos;
     joypos = joypos + center;
 
     if ( move.x || move.y ) { draw_cross(joypos); }
-    
+
     vibration = 0.0f;
     if (fire) {
                 draw_cross(joypos);
@@ -128,17 +128,18 @@ vec2 center,move,joypos;
 }
 
 void update(uint32_t time) {
-        if (pressed(button::DPAD_RIGHT) && speed > 20 )  { speed--; }
-        if (pressed(button::DPAD_LEFT)  && speed < 500 ) { speed++; }
-        if (pressed(button::B) || pressed(button::JOYSTICK)) { fire = 1; } else { fire = 0; }
-        if (pressed(button::X) || pressed(button::DPAD_UP)) { hyperspace = 1; } else { hyperspace = 0; }
-        if (pressed(button::DPAD_DOWN))  {
+        if (pressed(Button::DPAD_RIGHT) && speed > 20 )  { speed--; }
+        if (pressed(Button::DPAD_LEFT)  && speed < 500 ) { speed++; }
+        if (pressed(Button::B) || pressed(Button::JOYSTICK)) { fire = 1; } else { fire = 0; }
+        if (pressed(Button::X) || pressed(Button::DPAD_UP)) { hyperspace = 1; } else { hyperspace = 0; }
+        if (pressed(Button::DPAD_DOWN))  {
                 low_res = 1 - low_res;
                 if (low_res) {
-                        set_screen_mode(screen_mode::hires);
+                        set_screen_mode(ScreenMode::hires);
                         } else {
-                        set_screen_mode(screen_mode::lores);
+                        set_screen_mode(ScreenMode::lores);
                         }
                 }
 }
+
 
