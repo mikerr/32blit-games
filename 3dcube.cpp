7ad98e14d9b2b47958c3859723b47de2f9e79c46
Mike Redrobe <mike@redrobe.net>
2020-02-09 11:55:31 +0000

    Update for API changes

--- 3dcube.cpp
+++ 3dcube.cpp
@@ -5,42 +5,42 @@
 using namespace blit;
 
 typedef struct shape {
-    std::vector<vec3> points ;
+    std::vector<Vec3> points ;
 } shape;
 
 shape cube;
 
 int low_res;
 float zoom,spin;
-vec2 joypos,pos,dir,center;
-vec3 rot;
+Vec2 joypos,pos,dir,center;
+Vec3 rot;
 
-vec2 rotate_point(vec2 point,float angle){
-    mat3 t = mat3::identity();
-    t *= mat3::rotation(angle) ;
+Vec2 rotate_point(Vec2 point,float angle){
+    Mat3 t = Mat3::identity();
+    t *= Mat3::rotation(angle) ;
     point *= t;
     return (point);
 }
 
-vec3 rotate3d (vec3 point3d,vec3 rot) {
-    vec2 point;
+Vec3 rotate3d (Vec3 point3d,Vec3 rot) {
+    Vec2 point;
 
     // rotate 3d point about X
-    point = vec2(point3d.y,point3d.z);
+    point = Vec2(point3d.y,point3d.z);
     point = rotate_point (point,rot.x);
 
     point3d.y = point.x;
     point3d.z = point.y;
 
     // rotate 3d point about Y
-    point = vec2(point3d.z,point3d.x);
+    point = Vec2(point3d.z,point3d.x);
     point = rotate_point (point,rot.y);
 
     point3d.z = point.x;
     point3d.x = point.y;
 
     // rotate 3d point about Z
-    point = vec2(point3d.x,point3d.y);
+    point = Vec2(point3d.x,point3d.y);
     point = rotate_point (point,rot.z);
 
     point3d.x = point.x;
@@ -48,8 +48,8 @@ vec3 rotate3d (vec3 point3d,vec3 rot) {
 
     return (point3d);
 }
-vec2 to2d (vec3 point3d) {
-    vec2 point;
+Vec2 to2d (Vec3 point3d) {
+    Vec2 point;
 
     // project to screen
     int z = point3d.z - 100;
@@ -59,15 +59,15 @@ vec2 to2d (vec3 point3d) {
     return (point);
     }
 
-void draw_shape(shape shape,vec2 pos,float size){
-    vec3 p0 = shape.points[0];
+void draw_shape(shape shape,Vec2 pos,float size){
+    Vec3 p0 = shape.points[0];
     p0 = rotate3d(p0,rot);
 
-    vec2 lastpos = to2d(p0) * size;
+    Vec2 lastpos = to2d(p0) * size;
 
     for (auto &p: shape.points) {
         p = rotate3d(p,rot);
-        vec2 point = to2d(p) * size;
+        Vec2 point = to2d(p) * size;
         fb.line(lastpos + pos , point + pos);
         lastpos = point;
     }
@@ -75,40 +75,40 @@ void draw_shape(shape shape,vec2 pos,float size){
 
 
 void init() {
-    set_screen_mode(screen_mode::hires);
-    center = vec2(fb.bounds.w / 2, fb.bounds.h / 2);
+    set_screen_mode(ScreenMode::hires);
+    center = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
 
-    cube.points.push_back(vec3(-10,-10,-10));
-    cube.points.push_back(vec3(-10,-10,10));
-    cube.points.push_back(vec3(-10,10,10));
-    cube.points.push_back(vec3(10,10,10));
-    cube.points.push_back(vec3(10,-10,10));
-    cube.points.push_back(vec3(10,-10,-10));
-    cube.points.push_back(vec3(-10,-10,-10));
-    cube.points.push_back(vec3(-10,10,-10));
-    cube.points.push_back(vec3(-10,10,10));
-    cube.points.push_back(vec3(-10,10,-10));
-    cube.points.push_back(vec3(10,10,-10));
-    cube.points.push_back(vec3(10,10,10));
-    cube.points.push_back(vec3(10,10,-10));
-    cube.points.push_back(vec3(10,-10,-10));
+    cube.points.push_back(Vec3(-10,-10,-10));
+    cube.points.push_back(Vec3(-10,-10,10));
+    cube.points.push_back(Vec3(-10,10,10));
+    cube.points.push_back(Vec3(10,10,10));
+    cube.points.push_back(Vec3(10,-10,10));
+    cube.points.push_back(Vec3(10,-10,-10));
+    cube.points.push_back(Vec3(-10,-10,-10));
+    cube.points.push_back(Vec3(-10,10,-10));
+    cube.points.push_back(Vec3(-10,10,10));
+    cube.points.push_back(Vec3(-10,10,-10));
+    cube.points.push_back(Vec3(10,10,-10));
+    cube.points.push_back(Vec3(10,10,10));
+    cube.points.push_back(Vec3(10,10,-10));
+    cube.points.push_back(Vec3(10,-10,-10));
     
     low_res = 0;
     spin = 0.02;
     zoom = 3;
 
     pos = center;
-    dir = vec2 (1,2);
+    dir = Vec2 (1,2);
 }
 
 void render(uint32_t time) {
 
-    fb.pen(rgba(0, 0, 0));
-    fb.clear();
+    screen.pen(RGBA(0, 0, 0));
+    screen.clear();
 
-    fb.pen(rgba(255,255,255));
+    screen.pen(RGBA(255,255,255));
 
-    vec2 edge = center * 2;
+    Vec2 edge = center * 2;
     int cubesize = zoom * 10;
     if ((pos.x < cubesize ) || (pos.x > edge.x - cubesize )) { dir.x = -dir.x; spin = spin; }
     if ((pos.y < cubesize ) || (pos.y > edge.y - cubesize )) { dir.y = -dir.y; spin = spin; }
@@ -116,27 +116,27 @@ void render(uint32_t time) {
     pos = pos + dir;
     draw_shape (cube, pos, zoom);
 
-    rot += vec3(spin,spin,0);
+    rot += Vec3(spin,spin,0);
 }
 
 void update(uint32_t time) {
 
-    vec2 move = blit::joystick;
+    Vec2 move = blit::joystick;
     if (move.y > 0) zoom += 0.1;
     if (move.y < 0) zoom -= 0.1;
     pos.x += move.x;
 
-    if (pressed(button::DPAD_LEFT))  { spin -= 0.001;}
-    if (pressed(button::DPAD_RIGHT)) { spin += 0.001;}
+    if (pressed(Button::DPAD_LEFT))  { spin -= 0.001;}
+    if (pressed(Button::DPAD_RIGHT)) { spin += 0.001;}
 
-    if (pressed(button::X))  {
+    if (pressed(Button::X))  {
         low_res = 1 - low_res;
         if (low_res) {
-                set_screen_mode(screen_mode::hires);
-                center = vec2(fb.bounds.w / 2, fb.bounds.h / 2);
+                set_screen_mode(ScreenMode::hires);
+                center = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
                 } else {
-                set_screen_mode(screen_mode::lores);
-                center = vec2(fb.bounds.w / 2, fb.bounds.h / 2);
+                set_screen_mode(ScreenMode::lores);
+                center = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
                 }
         pos = center;
         }
