d24c5fb89c23cab28b620238ccd95c45e8125bd1
mikerr <mike@redrobe.net>
2021-02-23 17:02:40 +0000

    Added image tiles, as well as random dots

--- magiceye/magiceye.cpp
+++ magiceye/magiceye.cpp
@@ -6,27 +6,33 @@ using namespace blit;
 std::string status;
 Surface *tileimg,*depthimg,*fullscreenimg;
 
-int dpi = 44;
-float mu = 0.19 ; // depth of field
+bool moving = false;
+bool dots = true;
 
-void fill_with_tile (Surface *tile){
-	int tilewidth = tile->bounds.w ;
-	int tileheight = tile->bounds.h ;
-	for (int x=0; x<screen.bounds.w; x += tilewidth)
-	   for (int y=0; y<screen.bounds.h; y += tileheight)
-		   screen.stretch_blit(tile,Rect(0,0,tilewidth,tileheight),Rect(x,y,tilewidth,tileheight));
+int tilesize = 50;
+int tilenum;
+
+Pen colours[] = { Pen(0,0,0),Pen(255,0,0),Pen(0,255,0),Pen(0,0,255),Pen(255,255,255) };
+ 
+void fill_with_tile (Surface *target,Surface *tile,int tilesize,Rect tilerect){
+	for (int x=0; x<target->bounds.w; x += tilesize)
+	   for (int y=0; y<target->bounds.h; y += tilesize)
+		   target->stretch_blit(tile,tilerect,Rect(x,y,tilesize,tilesize));
 }
 
 int getdepth(int x,int y) {
 			auto p = depthimg->ptr(Vec2(x,y));
-			return ( p[0] != 0 );
+			int depth = 3 *  ( p[0] != 0 );
+			return ( depth );
 }
 
 void sirds() {
 
 #define round(X) (int)((X) + 0.5)
 
+int dpi = 44;
 int e = round(2.5 * dpi); // eye separation = 2.5 inches
+float mu = 0.08; // depth of field
 
 int screenwidth = screen.bounds.w;
 int screenheight = screen.bounds.h;
@@ -72,85 +78,99 @@ for (int y=0;y<screenheight;y++)  {
 				same[left] = right;
 			        }
 	         }
-	} // for x
+	} // end for x
 	int pix[screenwidth];
-	for (int x=screenwidth-1;x>0;x--) {
-		if (same[x] == x) pix[x] = rand ()  % 5;
-		else pix[x] = pix[same[x]];
-		
-		if (pix[x] == 0) screen.pen = Pen(0,0,0);
-		if (pix[x] == 1) screen.pen = Pen(255,0,0);
-		if (pix[x] == 2) screen.pen = Pen(0,255,0);
-		if (pix[x] == 3) screen.pen = Pen(0,255,255);
-		if (pix[x] == 4) screen.pen = Pen(255,255,255);
+	for (int i=0;i<screenwidth;i++)
+		pix[i]=i;
 
-		screen.pixel(Vec2(x,y));
+	for (int x=screenwidth-1;x>0;x--) {
+		if (dots) {
+			// random dots
+		    if (same[x] == x) pix[x] = rand ()  % 5;
+		    else pix[x] = pix[same[x]];
+
+		    screen.pen = colours[pix[x]];
+		    screen.pixel(Vec2(x,y));
+		} else {
+			//tiled background
+		    if (same[x] == x) pix[x] = x;
+		    else pix[x] = pix[same[x]];
+
+		    screen.blit(fullscreenimg,Rect(pix[x],y,1,1),Vec2(x,y));
+		}
 	   }
-    } // for y
+    } // end for y
+}
+
+Surface *make_surface (int width,int height, Surface *paletteimg) {
+    // make a surface and copy palette from another surface
+    uint8_t *data = new uint8_t[width * height];
+
+    Surface *newsurface = new Surface(data, PixelFormat::P, Size(width,height));
+    newsurface->palette = new Pen[256];
+    wmemcpy((wchar_t *)newsurface->palette, (wchar_t *)paletteimg->palette, 256 * sizeof(Pen));
+
+    return (newsurface);
 }
 
 void init() {
     set_screen_mode(ScreenMode::hires);
+
     depthimg = Surface::load(depthpic);
+    tileimg = Surface::load(tilepic);
+
+    fullscreenimg = make_surface(screen.bounds.w,screen.bounds.h,tileimg);
+ 
 }
+
 void render(uint32_t time) {
+
+    int x = (tilenum % 2);
+    int y = (tilenum >= 2);
+    Rect tilerect = Rect(64 * x, 64 * y,64,64);
+    fill_with_tile(fullscreenimg,tileimg,tilesize,tilerect);
+
     // Clear screen
     screen.pen = Pen (0,0,0);
     screen.clear();
 
-    srand(0);
+    // use same seed if static
+    if (!moving) srand(0);
     sirds();
 
-    screen.pen = Pen(0, 0, 0);
-    screen.rectangle(Rect(0, screen.bounds.h-10, screen.bounds.w, screen.bounds.h));
-    screen.pen = Pen (255,255,255);
-    std::string status = " DPI: " + std::to_string(dpi) + "  Depth: " + std::to_string(mu);
-    screen.text(status, minimal_font, Vec2(0, screen.bounds.h - 10));
+    // Reveal depth image
+    if (pressed(MENU)) {
+    	screen.pen = Pen (64,64,64);
+    	for (int x=0;x<screen.bounds.w;x++)  
+	    for (int y=0;y<screen.bounds.h;y++)  
+		    if (getdepth(x,y)) screen.pixel(Vec2(x,y));
+    }
 }
 	
 void update(uint32_t time) {
 	
-	if (pressed(MENU)) {
-    		screen.pen = Pen (64,64,64);
-    		for (int x=0;x<screen.bounds.w;x++)  
-		    for (int y=0;y<screen.bounds.h;y++)  
-			    if (getdepth(x,y)) screen.pixel(Vec2(x,y));
-	}
-	if (pressed(DPAD_DOWN)) {
+	tilesize += joystick.x * 2;
+	tilesize = std::max(25,tilesize);
+
+	//dep += joystick.y / 5;
+
+	if (pressed(DPAD_RIGHT)) { 
 		static uint32_t last;
 		if (now() - last > 100) { 
-			dpi--;
-			last = now();
+			tilenum++;  
+			if (tilenum > 3) tilenum = 0;
 		}
 	}
-	if (pressed(DPAD_UP)) {
-		static uint32_t last;
-		if (now() - last > 100) {
-			dpi++;
-			last = now();
-		}
-	}
-	if (pressed(DPAD_LEFT)) {
+	if (pressed(X)) { 
 		static uint32_t last;
 		if (now() - last > 100) { 
-			mu -= 0.01;
-			last = now();
+			dots = !dots; 
 		}
 	}
-	if (pressed(DPAD_RIGHT)) {
+	if (pressed(Y)) {
 		static uint32_t last;
 		if (now() - last > 100) { 
-			mu += 0.01;
-			last = now();
-		}
-	}
-	if (pressed(X)) {
-		static uint32_t last;
-		static bool res;
-		if (now() - last > 100) { 
-			if (res) set_screen_mode(ScreenMode::lores);
-			else set_screen_mode(ScreenMode::hires);
-			res = !res;
+			moving = !moving;
 		}
 	}
 }
