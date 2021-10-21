34fba539e90e92cc9b00461ca25a80d4cf284e28
mikerr <mike@redrobe.net>
2021-10-21 22:17:00 +0100

    T9 alphabet

--- keypad/keypad.cpp
+++ keypad/keypad.cpp
@@ -2,15 +2,18 @@
 #include "graphics/color.hpp"
 #include "types/vec2.hpp"
 
+//include "dictionary.h"
+
 using namespace blit;
 
-
-int low_res,fire,button_up;
 std::string passcode;
-Vec2 center,move,joypos;
+Vec2 center,joypos;
 
-void draw_cursor(Vec2 pos) {
-    screen.pen = Pen(255,255,255);
+char numbers [] = "123456789 0 ";
+std::string alpha [9] = { "   ","ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+
+void draw_cross(Vec2 pos) {
+    screen.pen = Pen(255,0,0);
     screen.line(pos + Vec2 (-5, -5), pos + Vec2 (5, 5) );
     screen.line(pos + Vec2 (5, -5), pos + Vec2 (-5, 5) );
 }
@@ -19,10 +22,13 @@ void draw_keypad() {
     int cel = screen.bounds.w / 5;
     Vec2 pos = center - Vec2(cel,cel * 1.5f);
 
-    for (int n=1; n<10;n++) {
-        screen.text(std::to_string(n), minimal_font, pos);
+    for (int n=0; n<9;n++) {
+    	screen.pen = Pen(255,255,255);
+        screen.text(std::string(1,numbers[n]), minimal_font, pos);
+    	screen.pen = Pen(128,128,128);
+        screen.text(alpha[n], minimal_font, pos + Vec2(0,13));
         pos.x += cel;
-	if (n % 3 == 0 ) { 
+	if ((n+1) % 3 == 0 ) { 
 		pos.x = center.x - cel;
 		pos.y += cel;
 		} 
@@ -32,7 +38,6 @@ void draw_keypad() {
 }
 
 char keypad_press () {     
-    char letters[] = "123456789 0 ";
     int cel = screen.bounds.w / 5;
     int letter=10;
 
@@ -40,61 +45,62 @@ char keypad_press () {
     int y = 3 + (joypos.y - center.y - cel * 1.5f) / cel;
 
     if (x >= 0 && x < 3)  { letter = x +(3 * y); }
-    return (letters[letter]);
+    return (letter);
 }
 
 void init() {
     set_screen_mode(ScreenMode::lores);
-    low_res = fire = button_up = 0;
     passcode = "";
 }
 
 void render(uint32_t time) {
-Vec2 cursorpos;
 
     screen.pen = Pen(0, 0, 0);
     screen.clear();
 
-    screen.pen = Pen(255,255,255);
-
     draw_keypad();
 
-    cursorpos = Vec2(0,screen.bounds.h - 20);
-    screen.text(passcode, minimal_font, cursorpos);
+    screen.text(passcode + "_", minimal_font, Vec2(0,0));
 
-    if (move.x || move.y) {
-    	draw_cursor(joypos);
+    draw_cross(joypos);
 
-    	screen.pen = Pen(255,0,0);
-	char letter = keypad_press();
-    	screen.text(std::string(1,letter), minimal_font, Vec2(0,0));
-    
-	if (button_up) {
-		passcode += letter;
-		button_up = 0;
-		}
-	}
+    screen.pen = Pen(255,0,0);
+    int num = keypad_press();
+    screen.text(std::string(1,numbers[num]), minimal_font, Vec2(0,screen.bounds.h - 10));
 }
 
 void update(uint32_t time) {
-	if (pressed(Button::B)) { fire = 1; } 
-        if (!pressed(Button::B)) {
-		if (fire == 1) { button_up = 1; }
-		fire = 0;
-		}
-	if (pressed(Button::DPAD_DOWN))  { 
-		low_res = 1 - low_res;
-		if (low_res) { 
-			set_screen_mode(ScreenMode::hires); 
-			} else { 
-			set_screen_mode(ScreenMode::lores);
-			}
-                }
-
+static int sub,lastnum;
+static uint32_t lasttime;
     center = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
-    move = blit::joystick;
 
-    joypos.x =  move.x * center.x ;
-    joypos.y =  move.y * center.y ;
+    joypos.x =  joystick.x * center.x ;
+    joypos.y =  joystick.y * center.y ;
+
+    if (pressed(Button::DPAD_LEFT))  { joypos.x = -0.5 * center.x; }
+    if (pressed(Button::DPAD_RIGHT)) { joypos.x = 0.5 * center.x; }
+    if (pressed(Button::DPAD_UP))    { joypos.y = -0.5 * center.y; }
+    if (pressed(Button::DPAD_DOWN))  { joypos.y = 0.3 * center.y; }
     joypos = joypos + center;
+
+    int num = keypad_press();
+    if (buttons.released & Button::Y) 
+	    if (passcode != "") passcode.pop_back();
+    if (buttons.released & Button::A) passcode += " ";
+    if (buttons.released & Button::X) passcode += numbers[num];
+    if (buttons.released & Button::B) {
+	    if (lastnum == num) {
+	    	    if ( now() - lasttime  < 1000) 
+		    {
+			if (passcode != "") passcode.pop_back();
+		    	sub++;
+		    }
+	    }
+	    else sub = 0;
+	    if (sub > alpha[num].size() - 1) sub = 0;
+	    char letter = alpha[num][sub];
+	    passcode += letter;
+	    lastnum = num;
+	    lasttime = now();
+    }
 }
