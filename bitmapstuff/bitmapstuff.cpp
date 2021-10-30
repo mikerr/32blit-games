fd86f170344d3fda08473fe30580bcba674c951f
mikerr <mike@redrobe.net>
2021-10-30 10:58:30 +0100

    Move variables out of render loop

--- bitmapstuff/bitmapstuff.cpp
+++ bitmapstuff/bitmapstuff.cpp
@@ -147,27 +147,18 @@ static int x,y=0;
     screen.pen = Pen (0,0,0);
     screen.clear();
 
-    if (demo) {
-	    zoom += 0.02f * dir;
-	    if (zoom > 1.0f || zoom < 0.1f) dir *= -1 ;
-    	    if (screen.bounds.w >= 320) {
-	   	 if (zoom < 0.1f) pic = 1 - pic;
-	    }
-    }
-
     if (speed >0) x += 3;
     else x -= 3;
 
-    	    if (screen.bounds.w >= 320) // on 32blit-console
-    			tilescreen(bricks,Point(x,y));
+    if (screen.bounds.w >= 320) // on 32blit-console
+    	tilescreen(bricks,Point(x,y));
 
     // Draw bitmap
     int blur = 1 + zoom;
     blit_rotate_sprite(pictures[pic],Rect(0,0,256,256),angle,blur,center);
-    uint32_t ms_end = now();
 
     // draw FPS meter
-    uint32_t ms = ms_end - ms_start;
+    uint32_t ms = now() - ms_start;
     if (ms == 0) ms = 1;
     status = std::to_string(1000 / ms) + " fps    " + std::to_string(ms) + " ms";
     screen.pen = Pen(255,255,255);
@@ -184,24 +175,23 @@ void update(uint32_t time) {
     if (pressed(Button::DPAD_DOWN) || joystick.y > 0.2f) zoom -= 0.01f;
 
     if (pressed(Button::A)) speed = 0;
-    if (buttons.released) demo = false;
     if (buttons.released & Button::Y) {
 		    pan = !pan;
 		    speed = 0;
     }
 
-    if (pressed(Button::MENU)) {
-    	screen.text("H E L P   M E N U", minimal_font, Vec2(50, 0));
-    	screen.text("Up/down    - Zoom in/out", minimal_font, Vec2(0 , 30));
-    	screen.text("Left/Right - Rotate", minimal_font, Vec2(0 , 40));
-    	screen.text("X: Toggle resolution", minimal_font, Vec2(50 , 60));
-    	screen.text("Y: Pan mode", minimal_font, Vec2(10 , 70));
-    	screen.text("A: Stop", minimal_font, Vec2(90 , 70));
-    	screen.text("B: Blur on/off", minimal_font, Vec2(50 , 80));
-    }
     if (buttons.released & Button::X) {
 	    	lowres = !lowres;
 	    	if (lowres) set_screen_mode(ScreenMode::lores);
 	    	else set_screen_mode(ScreenMode::hires);
 	    }
+
+    if (buttons.released) demo = false;
+    if (demo) {
+	    zoom += 0.005f * dir;
+	    if (zoom > 1.0f || zoom < 0.1f) dir *= -1 ;
+    	    if (screen.bounds.w >= 320) {
+	   	 if (zoom < 0.1f) pic = 1 - pic;
+	    }
+    }
 }
