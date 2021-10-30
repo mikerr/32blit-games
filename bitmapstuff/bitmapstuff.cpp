77f2caf7d2c646f13039348ce69f81aef66474ce
mikerr <mike@redrobe.net>
2021-10-30 10:47:38 +0100

    alternative integer based rotation

--- bitmapstuff/bitmapstuff.cpp
+++ bitmapstuff/bitmapstuff.cpp
@@ -10,8 +10,83 @@ bool demo,pan;
 Surface *pictures[2];
 Surface *bricks;
 
+
+typedef std::vector<Point> pointlist;
+
+pointlist getlinepoints (Point p1,Point p2) {
+int linelen=0;
+    int32_t dx = int32_t(abs(p2.x - p1.x));
+    int32_t dy = -int32_t(abs(p2.y - p1.y));
+
+    int32_t sx = (p1.x < p2.x) ? 1 : -1;
+    int32_t sy = (p1.y < p2.y) ? 1 : -1;
+
+    int32_t err = dx + dy;
+
+    pointlist linepoints;
+    Point p(p1);
+    while (linelen++ < 999){
+	// dont draw off screen
+        linepoints.push_back(p);
+        if ((p.x == p2.x) && (p.y == p2.y)) break;
+
+        int32_t e2 = err * 2;
+        if (e2 >= dy) { err += dy; p.x += sx; }
+        if (e2 <= dx) { err += dx; p.y += sy; }
+    }
+    return linepoints;
+}
+
+void texline (Surface *texture, Point p1,Point p2,int y) {
+    float x = 0;
+    pointlist allthepoints = getlinepoints (p1,p2);
+    // scale non-repeating texture to face width
+    float step = (float) texture->bounds.w / allthepoints.size(); 
+    for (auto p:allthepoints) {
+        if (p.x > 0 && p.y > 0 && p.x < screen.bounds.w && p.y < screen.bounds.h )
+		screen.blit(texture,Rect(x,y,2,2),p);
+	x += step;
+    }
+}
+void texquad(Surface *texture,Point t1,Point t2,Point t3,Point t4) {
+		   float y = 0;
+	           pointlist sideApoints = getlinepoints (t2,t1);
+	           pointlist sideBpoints = getlinepoints (t3,t4);
+		   int sblen = sideBpoints.size();
+		   float step = (float) texture->bounds.w / sblen;
+		   int sb = 0;
+		   for (auto p:sideApoints){
+			Point p1 = sideBpoints[sb];
+			texline(texture,p,p1,y);
+			if (++sb >= sblen) break;
+			y += step;
+		   }
+}
+
 void blit_rotate_sprite (Surface *sprite,Rect src, float angle, int blur, Vec2 screenpos) {
+
+Vec2 rot;
+    int width = src.w;
+    int height = src.h;
+
+    float sinangle = sin(angle);
+    float cosangle = cos(angle);
+    pointlist corners;
+    for (int x=0;x<width;x+=width-1)
+	    for (int y=0;y<height;y+=height-1) {
+		    int x1 = x - (width / 2);
+		    int y1 = y - (height / 2);
+		    rot.x = x1 * sinangle + y1 * cosangle;
+		    rot.y = y1 * sinangle - x1 * cosangle;
+		
+		    Vec2 pos = (rot * zoom ) + screenpos;
+		    corners.push_back(pos);
+    }
+    texquad(sprite, corners[0],corners[1],corners[3],corners[2]);
+}
+void blit_rotate_sprite_old (Surface *sprite,Rect src, float angle, int blur, Vec2 screenpos) {
 //rotate a sprite to screen at any angle
+// simple code, but heavy on floating point
 Vec2 rot;
     int width = src.w;
     int height = src.h;
@@ -47,6 +122,9 @@ void init() {
 	  //on 32blit-sonsole, not reduced RAM picosyation 
     	pictures[1] = Surface::load(lenaimg);
     	bricks = Surface::load(bricksimg);
+    } else {
+	    //PICO
+    	    set_screen_mode(ScreenMode::lores);
     }
 
     speed = 0.01;
@@ -71,9 +149,8 @@ static int x,y=0;
 
     if (demo) {
 	    zoom += 0.02f * dir;
-	    if (zoom > 3.0f || zoom < 0.1f) dir *= -1 ;
-    	    if (screen.bounds.w < 320) {
-	   	 // on picosystem, so reduced RAM
+	    if (zoom > 1.0f || zoom < 0.1f) dir *= -1 ;
+    	    if (screen.bounds.w >= 320) {
 	   	 if (zoom < 0.1f) pic = 1 - pic;
 	    }
     }
