a7ac8d74e9115e5aa9d52d39c9e50a5b2e80052c
mikerr <mike@redrobe.net>
2021-11-15 15:27:00 +0000

    credits and refineries

--- dune2/dune2.cpp
+++ dune2/dune2.cpp
@@ -11,11 +11,15 @@ Vec2 center,cursorpos;
 struct unit {
 	Vec2 pos;
 	Vec2 dest;
+	int health;
 };
 
-std::vector<unit> quads;
+std::vector<unit> quads, windtraps, refineries;
 
 Surface *backdrop,*dunesprites;
+
+Rect windtrap = Rect(0,19,32,30);
+Rect refinery = Rect(0,49,42,30);
 
 #define MAPSIZE 256
 
@@ -94,16 +98,29 @@ bool near (Vec2 cursor, Vec2 object) {
     return (distance < 20);
 }
 
+bool place_building ( Rect costume, std::vector<unit> &buildings) {
+Vec2 grid;
+	grid.x = int (cursorpos.x / 16) * 16;
+	grid.y = int (cursorpos.y / 16) * 16;
+	screen.blit(dunesprites,costume,grid);
+	if (clicked) {
+		clicked = false;
+		unit new_build;
+		new_build.pos = grid + Vec2(x,y);
+		buildings.push_back( new_build );
+		return (false);
+		}
+    return (true);
+}
+
 void init() {
     set_screen_mode(ScreenMode::hires);
-    clicked = 0;
     credits = 500;
 
     channels[0].waveforms = Waveform::WAVE;
     channels[0].wave_buffer_callback = &buffCallBack;  
 
     backdrop = Surface::load(map1);
-
     dunesprites = Surface::load(spritesheet);
 
     //spawn quads at random locations
@@ -115,14 +132,12 @@ void init() {
 	    quads.push_back(quad);
     }
 }
-
 void render(uint32_t time) {
-static int commanding,place_building,wind;
-int selected = -1;
+static int commanding=-1, placing_building, building_type;
 
-std::string status;
-static std::vector<Vec2> windtraps, refineries;
-static int building_type;
+static std::string status;
+Rect windtrap_pic = Rect (33,18,41,32);
+Rect refinery_pic = Rect (44,49,41,32);
 
     // Draw map
     screen.stretch_blit(backdrop,Rect(x/2,y/2,screen.bounds.w /2,screen.bounds.h /2),Rect(0,0,screen.bounds.w,screen.bounds.h));
@@ -133,10 +148,9 @@ static int building_type;
 	screen.text ("Y : show mini map", minimal_font, Vec2(50,20 + screen.bounds.h /2) );
 	screen.text ("A : comand unit", minimal_font, Vec2(50,30 + screen.bounds.h /2) );
     }
-    status = "";
 
     // Draw quads
-    int i = 0;
+    int thisquad = 0;
     for (auto &quad : quads) {
 
 	// move units to their destination, one step at a time
@@ -150,16 +164,31 @@ static int building_type;
 
 	if (near(cursorpos,quad.pos)) {
 		status = "Harkonnen quad unit";
-		selected = i;
+    		if (clicked) {
+			quad.dest = quad.pos;
+			commanding = thisquad;
+			clicked = 0;
+			status = "Yes, sir !";
+			play_wav(yessir,yessir_length);
+		}
 	}
 	// don't draw offscreen
-	if (quad.pos.x - x > 0 && quad.pos.y > 0 && quad.pos.x -x < screen.bounds.w && quad.pos.y - y < screen.bounds.h) {
+	if (quad.pos.x - x > 0 && quad.pos.y > 0 && quad.pos.x - x < screen.bounds.w && quad.pos.y - y < screen.bounds.h) {
 	   Rect quadsprite = Rect(0,0,16,16);
 	   blit_rotate_sprite(dunesprites,quadsprite,angle,Vec2(quad.pos.x - x, quad.pos.y - y));
-           if (i == selected) { draw_box(quad.pos.x - x - 8,quad.pos.y - y - 8,18,18); }
+           if (thisquad == commanding) { draw_box(quad.pos.x - x - 8,quad.pos.y - y - 8,18,18); }
 	}
-	i++;
+	thisquad++;
     }
+
+    // command quads
+    if (clicked && commanding != -1) {
+	    		clicked = 0;
+			status = "Acknowledged !";
+			play_wav(ack,ack_length);
+			quads[commanding].dest = Vec2(x,y) + cursorpos;
+			commanding = -1;
+		}
 
     // construction yard
     Vec2 yardpos = Vec2(160,160);
@@ -167,94 +196,59 @@ static int building_type;
     screen.blit(dunesprites,yard,yardpos - Vec2(x,y));
     if (near (cursorpos,yardpos)) {
 	    status = "Construction yard";
-		    Rect windtrap_pic = Rect (33,18,41,32);
-		    Rect refinery_pic = Rect (44,49,41,32);
 	            screen.blit(dunesprites,windtrap_pic,Vec2(screen.bounds.w-44,0));
 	            screen.text ("Windtrap", minimal_font,Vec2(screen.bounds.w-44,32));
-	            screen.blit(dunesprites,refinery_pic,Vec2(screen.bounds.w-44,42));
-	            screen.text ("Refinery", minimal_font,Vec2(screen.bounds.w-44,72));
+		    if (windtraps.size() > 0) {
+	            	screen.blit(dunesprites,refinery_pic,Vec2(screen.bounds.w-44,42));
+	            	screen.text ("Refinery", minimal_font,Vec2(screen.bounds.w-44,72));
 
-                    screen.pen = Pen(255,255,255);
-		    if (options) draw_box(screen.bounds.w-46,0,45,40);
-		    else draw_box(screen.bounds.w-46,38,45,40);
+		    	if (options) draw_box(screen.bounds.w-46,38,45,40);
+			else draw_box(screen.bounds.w-46,0,45,40);
+		    }
 
 	    if (clicked) {
-		    place_building = true;
+		    int cost;
 		    clicked = false;
 		    building_type = options;
+	    	    if (building_type == 0) { cost = 100; }
+	    	    if (building_type == 1) { cost = 300; }
+
+		    if (credits >= cost) {
+			    credits -= cost;
+			    placing_building = true;
+		    }
+		    else status = "Not enough credits!";
 	    }
     }
-    // Windtrap generators
-    Rect windtrap = Rect(0,19,32,30);
-    Rect refinery = Rect(0,49,42,30);
-    if (place_building && pressed(Button::B)) place_building = false;
 
-    if (place_building && building_type == 1) {
-	            Vec2 grid ;
-		    grid.x = int (cursorpos.x / 32) * 32;
-		    grid.y = int (cursorpos.y / 32) * 32;
-		    screen.blit(dunesprites,windtrap,grid);
-		    if (clicked) {
-			    place_building = false;
-			    windtraps.push_back( grid + Vec2(x,y));
-		    }
-    }
-    if (place_building && building_type == 0) {
-	            Vec2 grid ;
-		    grid.x = int (cursorpos.x / 32) * 32;
-		    grid.y = int (cursorpos.y / 32) * 32;
-		    screen.blit(dunesprites,refinery,grid);
-		    if (clicked) {
-			    place_building = false;
-			    refineries.push_back( grid + Vec2(x,y));
-		    }
+    if (placing_building) {
+	    if (building_type == 0) { placing_building = place_building (windtrap,windtraps); }
+       	    if (building_type == 1) { placing_building = place_building (refinery,refineries); }
+    	    if (pressed(Button::B)) placing_building = false;
     }
 
     for (auto r : refineries) {
-    	screen.blit(dunesprites,refinery,r - Vec2(x,y));
-    	if (near (cursorpos,r)) {
-		status = "Spice Refinery";
-	}
+    	screen.blit(dunesprites,refinery, r.pos - Vec2(x,y));
+    	if (near (cursorpos,r.pos)) { status = "Spice Refinery"; }
     }
 
     for (auto w : windtraps) {
-    	screen.blit(dunesprites,windtrap,w - Vec2(x,y));
-    	if (near (cursorpos,w)) {
-		status = "Windtrap generator";
-	}
-    }
-
-    if (time % 2000 == 0) { 
-	    	    status = "Warning, Enemy unit approaching";
-		    //play_wav(enemyunit,enemyunit_length);
-		    }
-
-    if (clicked) {
-		clicked = 0;
-
-		if (commanding != -1) {
-			status = "Acknowledged !";
-			play_wav(ack,ack_length);
-			quads[commanding].dest = Vec2(x,y) + cursorpos;
-			commanding = -1;
-		}
-		if (selected != -1) {
-			status = "Yes, sir !";
-			play_wav(yessir,yessir_length);
-			commanding = selected;
-			}
+    	screen.blit(dunesprites,windtrap, w.pos - Vec2(x,y));
+    	if (near (cursorpos,w.pos)) { status = "Windtrap generator"; }
     }
 
     draw_cursor(cursorpos);
+
     // status text
-    screen.pen = Pen(255,255,255);
     Vec2 screenbottom = Vec2(0,screen.bounds.h - 10);
+    screen.text ("Credits: " + std::to_string(credits), minimal_font,Vec2(0,0));
     screen.text(status, minimal_font, screenbottom);
 
     // Draw mini map
     if (pressed(Button::Y))  {
 	    int mmsize = screen.bounds.w / 2.5;
-	    screen.stretch_blit(backdrop,Rect(1,0,MAPSIZE,MAPSIZE),Rect(screen.bounds.w-mmsize,screen.bounds.h-mmsize,mmsize,mmsize));
+	    Rect bottomright = Rect(screen.bounds.w-mmsize,screen.bounds.h-mmsize,mmsize,mmsize);
+	    screen.stretch_blit(backdrop,Rect(1,0,MAPSIZE,MAPSIZE),bottomright);
     }
 }
 
@@ -269,32 +263,24 @@ static Vec2 joypos;
     if (pressed(Button::DPAD_UP))    joypos.y -= cspeed;
     if (pressed(Button::DPAD_DOWN))  joypos.y += cspeed;
 
-    joypos.x = std::min(joypos.x,center.x);
-    joypos.y = std::min(joypos.y,center.y);
-    joypos.x = std::max(joypos.x,-center.x);
-    joypos.y = std::max(joypos.y,-center.y);
+    joypos.x = std::clamp(joypos.x,-center.x,center.x);
+    joypos.y = std::clamp(joypos.y,-center.y,center.y);
 
     cursorpos = joypos + center;
 
-    int SCROLLSPEED = 1;
-    if ( cursorpos.x < 10 ) x -= SCROLLSPEED;
-    if ( cursorpos.x > screen.bounds.w - 10 ) x += SCROLLSPEED;
-    if ( cursorpos.y < 10 ) y -= SCROLLSPEED;
-    if ( cursorpos.y > screen.bounds.h - 10) y += SCROLLSPEED;
+    if ( cursorpos.x < 10 ) x--;
+    if ( cursorpos.y < 10 ) y--;
+    if ( cursorpos.x > screen.bounds.w - 10 ) x++;
+    if ( cursorpos.y > screen.bounds.h - 10 ) y++;
 
-    int width  = MAPSIZE * 2 - screen.bounds.w; 
-    int height = MAPSIZE * 2 - screen.bounds.h; 
-
-    x = std::min(x,width);
-    y = std::min(y,height);
-    x = std::max(0,x);
-    y = std::max(0,y);
+    x = std::clamp(x,0,(int)(MAPSIZE * 2 - screen.bounds.w));
+    y = std::clamp(y,0,(int)(MAPSIZE * 2 - screen.bounds.h));
 
     if (buttons.released & Button::A) { clicked = 1; }
     if (buttons.released & Button::X) {
-	    if (lowres) set_screen_mode(ScreenMode::lores);
-	    else set_screen_mode(ScreenMode::hires);
 	    lowres = !lowres;
+	    if (lowres) set_screen_mode(ScreenMode::lores); 
+	    else set_screen_mode(ScreenMode::hires);
 	    }
     if (buttons.released & Button::B) options = !options;
 }
