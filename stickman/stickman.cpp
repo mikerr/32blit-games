f624e7936242891713baffdd0411975493d8741d
mikerr <mike@redrobe.net>
2021-12-10 17:04:24 +0000

    Better arm movement

--- stickman/stickman.cpp
+++ stickman/stickman.cpp
@@ -1,20 +1,11 @@
 #include "32blit.hpp"
-#include "graphics/color.hpp"
-#include "types/vec2.hpp"
 
 using namespace blit;
 
-typedef struct shape {
-    std::vector<Vec3> points ;
-} shape;
+int speed;
+float dustx,count;
 
-shape stickman;
-
-int rot;
-float x,count;
-Vec2 center;
-
-Vec2 neck,elbow,wrist,hip,knee,ankle;
+Vec2 center,neck,hip;
 
 Pen black = Pen(0,0,0);
 Pen white = Pen(255,255,255);
@@ -27,13 +18,17 @@ void drawjoint (Vec2 start, Vec2 end) {
     start += center;
     end += center;
     screen.line (start,end);
-    if (pressed(Y)) screen.circle(end,5);
+    if (pressed(Y)) {
+	    screen.circle(start,5);
+	    screen.circle(end,5);
+    }
 }
 
 void drawleg(int phase) {
 float sinc;
+Vec2 knee,ankle;
 
-    sinc = sin(count + phase) * 20;
+    sinc = sin(count + phase) * 30;
     sinc = deg2rad(sinc +170);
 
     knee = Vec2 (sin (sinc), -cos (sinc)) * 50;
@@ -49,21 +44,21 @@ float sinc;
 }
 
 void drawarm(int phase) {
-float sinc;
+float sinc,cosc;
+Vec2 shoulder,elbow,wrist;
 
-Vec2 shoulder = neck + Vec2(0,15);
+    shoulder = neck + Vec2(0,10);
 
     drawjoint (shoulder,neck);
 
-    sinc = (sin(count + phase) * 40);
-    sinc = deg2rad(sinc - 20);
+    sinc = deg2rad(sin(count + phase) * 60);
 
     elbow = Vec2 (sin (sinc), cos (sinc)) * 30;
     elbow += shoulder;
     drawjoint (shoulder,elbow);
 
-    sinc = sin (count + phase + 0.873f + 1) * 15;
-    sinc = deg2rad(sinc + 90);
+    sinc = sin(count + phase) * 60;
+    sinc = deg2rad(sinc + 50);
 
     wrist = Vec2 (sin (sinc), cos (sinc)) * 30;
     wrist += elbow;
@@ -85,10 +80,21 @@ void drawtorso(void) {
     screen.pen = white;
 }
 
+void drawground ( void) {
+
+    for(int dust=0; dust < screen.bounds.w + 200; dust += 40) {
+            screen.pen = Pen(225,225,225);
+	    screen.pixel (Vec2(dustx + dust,220));
+            screen.pen = Pen(200,200,200);
+	    screen.pixel (Vec2(dustx/2 + dust,200));
+            screen.pen = Pen(160,160,160);
+	    screen.pixel (Vec2(dustx/4 + dust,180));
+    }
+}
 void init() {
     set_screen_mode(ScreenMode::hires);
     center = Vec2(screen.bounds.w / 2, screen.bounds.h / 2);
-    rot = 100;
+    speed = 100;
 }
 
 void render(uint32_t time) {
@@ -98,8 +104,10 @@ float left,right;
     screen.pen = black;
     screen.clear();
 
+    drawground();
+
     screen.pen = white;
- 
+
     drawleg(left);
     drawleg(right);
 
@@ -107,23 +115,14 @@ float left,right;
 
     drawarm(left);
     drawarm(right);
-
-    for(int dust=0; dust < screen.bounds.w + 200; dust += 40) {
-            screen.pen = Pen(225,225,225);
-	    screen.pixel (Vec2(x + dust,220));
-            screen.pen = Pen(200,200,200);
-	    screen.pixel (Vec2(x/2 + dust,200));
-            screen.pen = Pen(160,160,160);
-	    screen.pixel (Vec2(x/4 + dust,180));
-    }
-
 }
+
 void update(uint32_t time) {
-    count += rot / 2000.0f;
+    count += speed / 2000.0f;
 
-    x -= rot / 50.0f;
-    if (x < -160) x = 0;
+    dustx -= speed / 50.0f;
+    if (dustx < -160) dustx = 0;
 
-    if (pressed(DPAD_LEFT) && rot > 20) rot--;
-    if (pressed(DPAD_RIGHT) && rot < 900) rot++;
+    if (pressed(DPAD_LEFT) && speed > 20) speed--;
+    if (pressed(DPAD_RIGHT) && speed < 900) speed++;
 }
