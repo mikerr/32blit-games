9dc0b221abf266f644737f634db11cde8e00134a
mikerr <mike@redrobe.net>
2023-03-06 20:39:53 +0000

    removing simplechess engine

--- rookery/rookery.cpp
+++ rookery/rookery.cpp
@@ -5,32 +5,17 @@ using namespace blit;
 #define BOARDSIZE 220
 Surface *boardimage, *allpieces;
 Vec2 boardpos, cursor;
+int bstyle;
+float zoom; 
 
 typedef struct pieceobj {
 	Rect pic;
 	Vec2 pos;
+	char type;
 	int selected;
 
-} piece;
+} pieceobj;
 std::vector <pieceobj> pieces;
-
-#include "SimpleChess.h"
-Game g;
-void randomMove(Game &g) {
-	// get all pieces that are allowed to move
-	std::vector<Piece*> moveable_pieces;
-	pieceColor player = g.currentPlayer();
-	for (unsigned i=0; i < 8; ++i) {
-		for (unsigned j=0; j < 8; ++j) {
-			Piece* p = g.getPiece(i, j);
-			if (p->color == player && !p->legalMoves.empty())
-				moveable_pieces.push_back(p);
-		}
-	}
-	// pick one and make a random legal move with it
-	Piece* p = moveable_pieces.at( rand() % moveable_pieces.size());
-	g.move(p->legalMoves.at( rand () % p->legalMoves.size()) );
-}
 
 // grid coords to screen
 Vec2 grid2screen(int a, int b){
@@ -43,7 +28,7 @@ Vec2 grid2screen(int a, int b){
 
 // Return sprite image from chess notation e.g. N = black knight image
 Rect getimage (char p) {
-    Rect img = Rect(0,0,10,10);
+    Rect img = Rect(0,0,20,20);
 
     if (p == 'R') img = Rect(21,76,18,28);
     if (p == 'r') img = Rect(21,22,18,28);
@@ -67,6 +52,7 @@ void set_boardline(std::string boardline, int y) {
     for (int x=0;x < 8; x++){
 	    char p = boardline[x];
 	    piece.pos = Vec2(x,y);
+	    piece.type = p;
 	    piece.pic = getimage(p);
 	    piece.selected = 0;
     	    pieces.push_back(piece);
@@ -78,24 +64,45 @@ void screenrect (Point a, Point b, Point c, Point d) {
     screen.line(a,b); screen.line(b,c); screen.line(c,d); screen.line(d,a);
 }
 
+void draw_cursor(Vec2 cursor) {
+    // draw border round selected square
+    
+    Vec2 a = grid2screen(cursor.x,cursor.y);
+    Vec2 b = grid2screen(cursor.x+1,cursor.y);
+    Vec2 c = grid2screen(cursor.x+1,cursor.y+1);
+    Vec2 d = grid2screen(cursor.x,cursor.y+1);
+			  
+    screen.pen = Pen(255,255,255);
+    screenrect (a,b,c,d);
+}
+
+void help_screen() {
+   	   screen.pen = Pen(255,255,255);
+	   screen.text("Controls",minimal_font,Vec2(10,60));
+	   screen.text("A: move piece",minimal_font,Vec2(10,80));
+	   screen.text("B: cancel move",minimal_font,Vec2(10,100));
+	   screen.text("X: change board style",minimal_font,Vec2(10,120));
+	   screen.text("Y: swap sides",minimal_font,Vec2(10,140));
+}
+
 void init() {
 
     set_screen_mode(ScreenMode::hires);
 
-    boardimage = Surface::load(board2);
+    boardimage = Surface::load(board1);
     allpieces = Surface::load(pieces1);
 
     // setup board
-    /*
     set_boardline ( "RNBKQBNR",0);
     set_boardline ( "PPPPPPPP",1);
     set_boardline ( "pppppppp",6);
     set_boardline ( "rnbkqbnr",7);
-    */
 
+    cursor = Vec2(4,6);
 }
 
 void render(uint32_t time) {
+static uint32_t start;	
 
     screen.pen = Pen(0,0,0);
     screen.clear();
@@ -111,79 +118,55 @@ void render(uint32_t time) {
 		screen.stretch_blit(allpieces, piece.pic, Rect(screenpos.x,screenpos.y,22,28));
 		}
 
-    // draw border round selected square
-    Vec2 a = grid2screen(cursor.x,cursor.y);
-    Vec2 b = grid2screen(cursor.x+1,cursor.y);
-    Vec2 c = grid2screen(cursor.x+1,cursor.y+1);
-    Vec2 d = grid2screen(cursor.x,cursor.y+1);
-			  
-    screen.pen = Pen(255,255,255);
-    screenrect (a,b,c,d);
+   draw_cursor(cursor);
 
-    for (int x=0;x<8;x++)
-	    for (int y=0;y<8;y++) {
-		    Piece p = g.getPiece(y,x);
-		    Rect pic = Rect(0,0,10,10);
-		    if (p.name == pieceName::PAWN && p.color == pieceColor::WHITE) pic = getimage('P');
-		    if (p.name == pieceName::PAWN && p.color == pieceColor::BLACK) pic = getimage('p');
-
-		    if (p.name == pieceName::ROOK && p.color == pieceColor::WHITE) pic = getimage('R');
-		    if (p.name == pieceName::ROOK && p.color == pieceColor::BLACK) pic = getimage('r');
-
-		    if (p.name == pieceName::KNIGHT && p.color == pieceColor::WHITE) pic = getimage('N');
-		    if (p.name == pieceName::KNIGHT && p.color == pieceColor::BLACK) pic = getimage('n');
-
-		    if (p.name == pieceName::BISHOP && p.color == pieceColor::WHITE) pic = getimage('B');
-		    if (p.name == pieceName::BISHOP && p.color == pieceColor::BLACK) pic = getimage('b');
-
-		    if (p.name == pieceName::KING && p.color == pieceColor::WHITE) pic = getimage('K');
-		    if (p.name == pieceName::KING && p.color == pieceColor::BLACK) pic = getimage('k');
-
-		    if (p.name == pieceName::QUEEN && p.color == pieceColor::WHITE) pic = getimage('Q');
-		    if (p.name == pieceName::QUEEN && p.color == pieceColor::BLACK) pic = getimage('q');
-
-		    Vec2 screenpos = grid2screen(x,y);
-		    screen.stretch_blit(allpieces, pic, Rect(screenpos.x,screenpos.y,22,28));
-	    }
-    switch(g.state) {
-		case gameState::DRAW: 
-			screen.text("Game is a DRAW",minimal_font,Vec2(100,100)); break;
-		case gameState::WON_WHITE: 
-			screen.text("WHITE won !",minimal_font,Vec2(100,100)); break;
-		case gameState::WON_BLACK: 
-			screen.text("BLACK won !",minimal_font,Vec2(100,100)); break;
-		default: break;
-	}
+   if (time - start < 3000) help_screen();
 }
-
-
 
 void update(uint32_t time) {
 static uint32_t lasttime,lasttime2;
+
      if (time - lasttime2 > 150) {
-	     if (g.state == gameState::PLAYING) { randomMove(g); }
 	     lasttime2 = time;
      }
-     if ( time - lasttime > 70) {
-	if (pressed(Button::DPAD_LEFT)) 
-		if ( cursor.x > 0 ) cursor.x--;
-	if (pressed(Button::DPAD_RIGHT)) 
-		if ( cursor.x < 7) cursor.x++;
-	if (pressed(Button::DPAD_UP))
-		if ( cursor.y > 0 ) cursor.y--;
-	if (pressed(Button::DPAD_DOWN))
-		if ( cursor.y < 7) cursor.y++;
+
+    if (time - lasttime > 70) {
+	if (pressed(Button::DPAD_LEFT) &&  cursor.x > 0 ) cursor.x--;
+	if (pressed(Button::DPAD_RIGHT) && cursor.x < 7) cursor.x++;
+	if (pressed(Button::DPAD_UP) && cursor.y > 0 ) cursor.y--;
+	if (pressed(Button::DPAD_DOWN) && cursor.y < 7) cursor.y++;
 	lasttime = time;
 	}
+
+    // start/finish move ... pickup drop piece
     if (buttons.released & Button::A) {
 		for ( auto &p : pieces) 
 			if (p.pos == cursor) p.selected = 1;
 			else {
-				if (p.selected) p.pos = cursor;
+				//drop - move the piece
+				if (p.selected ) p.pos = cursor;
 				p.selected = 0;
 				}
 		}
+    //cancel move
     if (buttons.released & Button::B) 
 		for ( auto &p : pieces) p.selected = 0;
+
+    //flip board round
+    if (buttons.released & Button::Y) {
+    		for (auto &p: pieces) {
+			p.pos = Vec2(7,7) - p.pos;
+		}
+    }
+    //change board image
+    if (buttons.released & Button::X) {
+	    	bstyle = !bstyle;
+		// free up memory
+		delete[] boardimage->data;
+		delete[] boardimage->palette;
+		//swap the board image
+		if (bstyle)  boardimage = Surface::load(board2);
+		else boardimage = Surface::load(board1);
+    }
 }
 
